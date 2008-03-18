b2994b757a50e5cfba02392d88bcfcf6009764a0
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2008-03-18 21:17:17 +0000

    formatting
    git-svn-id: http://svn.silverstripe.com/open/modules/flickrservice/trunk@51321 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/FlickrGallery.php
+++ code/FlickrGallery.php
@@ -1,46 +1,46 @@
 <?php
- 
 class FlickrGallery extends Page {
  
-   // define your database fields here - for example we have author
-   static $db = array(
-   	"User" => "Varchar",
-   	"GroupID" => "Varchar", // This is the group ID, not the group NAME. It's in the format 377682@N20
-   	"Method" => "Int",
-   	"Photoset" => "Varchar",
-   	"NumberToShow" => "Int",
-   	"Tags" => "Varchar(200)",
-   	"Sortby" => "Varchar"
-   );
+	// define your database fields here - for example we have author
+	static $db = array(
+	   	"User" => "Varchar",
+	   	"GroupID" => "Varchar", // This is the group ID, not the group NAME. It's in the format 377682@N20
+	   	"Method" => "Int",
+	   	"Photoset" => "Varchar",
+	   	"NumberToShow" => "Int",
+	   	"Tags" => "Varchar(200)",
+	   	"Sortby" => "Varchar"
+	);
    
-   static $defaults = array(
+	static $defaults = array(
 		"Method" => 2,
 		"NumberToShow" => 20,
 		"Sortby" => "date-posted-desc"
 	);
    
-  static $icon = "flickrservice/images/flickr";
+	static $icon = "flickrservice/images/flickr";
  
-   // add custom fields for this flickr gallery page
-   function getCMSFields($cms) {
-   	  Requirements::javascript( 'flickrservice/javascript/FlickrGallery_CMS.js' );
+	// add custom fields for this flickr gallery page
+	function getCMSFields($cms) {
+		Requirements::javascript( 'flickrservice/javascript/FlickrGallery_CMS.js' );
    	  
-      $fields = parent::getCMSFields($cms);
-      $fields->addFieldToTab("Root.Content.Photos", new DropdownField("Method", "Select ", array(
+		$fields = parent::getCMSFields($cms);
+		$fields->addFieldToTab("Root.Content.Photos", new DropdownField("Method", "Select ", array(
 				'1' => 'Photos taken by',
 				'2' => 'Photos tagged with',
 				'3' => 'Photos from photoset',
 				'4' => 'Photos from group'
-	  )));
-      $fields->addFieldToTab("Root.Content.Photos", new TextField("User","Flickr User"));
-      $fields->addFieldToTab("Root.Content.Photos", new TextField("GroupID", "Group ID (see documentation for help)"));
-      $fields->addFieldToTab("Root.Content.Photos", new TextField("Tags","Tags"));
-      $fields->addFieldToTab("Root.Content.Photos", new TextField("Photoset","Photoset id"));
-      $fields->addFieldToTab("Root.Content.Photos", new NumericField("NumberToShow","Photos per page", "20"));
-      $fields->addFieldToTab("Root.Content.Photos", new DropdownField("Sortby", "Sort by ", array(
+		)));
+		$fields->addFieldToTab("Root.Content.Photos", new TextField("User","Flickr User"));
+		$fields->addFieldToTab("Root.Content.Photos", new TextField("GroupID", "Group ID (see documentation for help)"));
+		$fields->addFieldToTab("Root.Content.Photos", new TextField("Tags","Tags"));
+		$fields->addFieldToTab("Root.Content.Photos", new TextField("Photoset","Photoset id"));
+		$fields->addFieldToTab("Root.Content.Photos", new NumericField("NumberToShow","Photos per page", "20"));
+		$fields->addFieldToTab("Root.Content.Photos", new DropdownField("Sortby", "Sort by ", array(
 				'date-posted-desc' => 'Most recent',
 				'interestingness-desc' => 'Most interesting')));
-      return $fields;
+	
+		return $fields;
    }
    
    function FlickrPhotos(){
@@ -60,10 +60,8 @@ class FlickrGallery extends Page {
 			case 4:
 				$photos = $flickr->getPhotosFromGroupPool($this->GroupID, $this->Tags, $this->User, $this->NumberToShow, $page, $this->SortBy);
 				break;
-			}
+		}
 			
-		//Debug::show($photos);
-		
 		$photoHTML = "<div class='flickr' style='float:left'>";
 		foreach($photos->PhotoItems as $photo){
 			$caption = htmlentities("<a href='$photo->page_url'>View this in Flickr</a>");
@@ -74,12 +72,12 @@ class FlickrGallery extends Page {
 	 if($photos->PhotoItems){
 		$photoHTML .= "<div class='pages'><div class='paginator'>";
 		$photoHTML .= $photos->getPages();
-	$photoHTML .= "</div><span class='results'>(".$photos->getTotalPhotos()." Photos)</span></div>";
-	}
-	else {
-	
-	$photoHTML .= "<span>Sorry!  Gallery doesn't contain any images for this page.</span>";
-	}
+		$photoHTML .= "</div><span class='results'>(".$photos->getTotalPhotos()." Photos)</span></div>";
+		}
+		else {
+		
+		$photoHTML .= "<span>Sorry!  Gallery doesn't contain any images for this page.</span>";
+		}
 		
 		return $photoHTML;
 	}
@@ -87,32 +85,32 @@ class FlickrGallery extends Page {
 
 class FlickrGallery_Controller extends Page_Controller {
 	function init() {
-      if(Director::fileExists(project() . "/css/FlickrGallery.css")) {
-         Requirements::css(project() . "/css/FlickrGallery.css");
-      }else{
-         Requirements::css("flickrservice/css/FlickrGallery.css");
-      }
-      Requirements::javascript( "flickrservice/javascript/prototype.js" );
+	if(Director::fileExists(project() . "/css/FlickrGallery.css")) {
+	   Requirements::css(project() . "/css/FlickrGallery.css");
+	} else {
+	   Requirements::css("flickrservice/css/FlickrGallery.css");
+	}
+	
+	Requirements::javascript( "flickrservice/javascript/prototype.js" );
 	  Requirements::javascript( "flickrservice/javascript/effects.js" );
 	  Requirements::javascript( "flickrservice/javascript/lightwindow.js" );
 	
-      Requirements::css("flickrservice/css/lightwindow.css");
-      
-      if( $pos = strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'MSIE' ) ) {
-      		$version = substr( $_SERVER[ 'HTTP_USER_AGENT' ], $pos + 5, 3 );
-      		if( $version < 7 ) {
+	Requirements::css("flickrservice/css/lightwindow.css");
+	
+	if( $pos = strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'MSIE' ) ) {
+			$version = substr( $_SERVER[ 'HTTP_USER_AGENT' ], $pos + 5, 3 );
+			if( $version < 7 ) {
 					Requirements::css( "flickrservice/css/lightwindowIE6.css" );
-      		}
-      	}
-      
-      parent::init();	
+			}
+		}
+	
+	parent::init();	
    }
    
    function Content(){
 			return $this->Content.$this->FlickrPhotos();
    }
-
+   
 }
-
 
 ?>
