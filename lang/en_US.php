768a264ba7713c3ffe6001a4144aa17254ffc1bd
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-01-12 04:41:53 +0000

    MINOR Added language master table
    git-svn-id: http://svn.silverstripe.com/open/modules/flickrservice/trunk@70002 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- lang/en_US.php
+++ lang/en_US.php
@@ -12,41 +12,6 @@ $lang['en_US']['FlickrGallery']['SINGULARNAME'] = array(
 	50,
 	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
 );
-$lang['en_US']['FlickrGallery']['db_GroupID'] = array(
-	'GroupID',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['FlickrGallery']['db_Method'] = array(
-	'Method',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['FlickrGallery']['db_NumberToShow'] = array(
-	'NumberToShow',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['FlickrGallery']['db_Photoset'] = array(
-	'Photoset',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['FlickrGallery']['db_Sortby'] = array(
-	'Sortby',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['FlickrGallery']['db_Tags'] = array(
-	'Tags',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['FlickrGallery']['db_User'] = array(
-	'User',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
 $lang['en_US']['FlickrWidget']['PLURALNAME'] = array(
 	'Flickr Widgets',
 	50,
@@ -56,21 +21,6 @@ $lang['en_US']['FlickrWidget']['SINGULARNAME'] = array(
 	'Flickr Widget',
 	50,
 	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
-);
-$lang['en_US']['FlickrWidget']['db_NumberToShow'] = array(
-	'NumberToShow',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['FlickrWidget']['db_Sortby'] = array(
-	'Sortby',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['FlickrWidget']['db_User'] = array(
-	'User',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
 );
 
 ?>
